137ea91cd80df8ef16d75d03e6ac4d8f18c66084
chaotixNLD <205326717+ChaotixNLD@users.noreply.github.com>
2025-04-03 09:11:34 +0200

    created basic function to create the grid adjusted CSS, so only inner div's have a border

--- javascript.js
+++ javascript.js
@@ -1,1 +1,15 @@
-console.log("hello world")
+const container = document.getElementById("container")
+createGrid(16)
+
+function createGrid(num){
+for(let i = 1; i <=num; i++){
+    let column = document.createElement("div")
+    column.classList.add("column")
+    container.appendChild(column)
+    for(let j = 1; j <= num; j++){
+        let row = document.createElement("div")
+        row.classList.add("row")
+        column.appendChild(row)
+    }
+}
+}
